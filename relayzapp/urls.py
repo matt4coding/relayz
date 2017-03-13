2d14f292138373173938046cee9b5555b4089292
matt4coding <Matt4Coding@gmail.com>
2017-03-12 20:29:43 -0400

    created home page URL conf, modified base template

--- relayzapp/urls.py
+++ relayzapp/urls.py
@@ -1,10 +1,39 @@
 from django.conf.urls import patterns, include, url
-from django.contrib import admin
+from marketing.views import HomePage
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'relayzapp.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+
+    # Marketing pages
+    url(r'^$', HomePage.as_view(), name="home"),
+
+    # Subscriber related URLs
+
+
+    # Admin URL
+
+
+    # Login/Logout URLs
+
+
+    # Account related URLs
+
+
+    # Contact related URLS
+
+
+    # Communication related URLs
+
 )
